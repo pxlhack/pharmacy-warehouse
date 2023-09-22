bd7c2ffd9c3c2a1c8758c15dd4aa93b95f18e57f
pxlhack <nellsouhd@gmail.com>
2023-09-22 23:02:53 +0700

    added Pharmacy model

---
+++ models/Pharmacy.h
@@ -0,0 +1,19 @@
+#ifndef LAB1_PHARMACY_H
+#define LAB1_PHARMACY_H
+
+#include <string>
+
+using namespace std;
+
+
+class Pharmacy {
+
+private:
+    int id;
+    string name;
+    string address;
+    string phoneNumber;
+};
+
+
+#endif //LAB1_PHARMACY_H
