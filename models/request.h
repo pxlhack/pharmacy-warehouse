859925abe05df7da773fa82dee0ec327e6571197
pxlhack <nellsouhd@gmail.com>
2023-09-25 16:42:50 +0700

    added findAll, toStringVector for Request

--- models/request.h
+++ models/request.h
@@ -32,11 +32,51 @@ public:
         return this->id;
     }
 
+
+    static vector<Request> findAll(SQLHDBC sqlhdbc) {
+        string selectSql = "SELECT * FROM requests;";
+        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, selectSql);
+
+        vector<Request> requests;
+        for (const auto &row: results) {
+            Request request = parseFromVector(row);
+            requests.push_back(request);
+        }
+        return requests;
+    }
+
+    vector<string> toStringVector(SQLHDBC sqlhdbc) {
+        ostringstream oss;
+        oss << "SELECT name FROM pharmacies WHERE id = " << pharmacyId << ";";
+        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, oss.str());
+        if (results.empty()) {
+            throw runtime_error("Pharmacy not found");
+        }
+
+        return {to_string(id), creationDate.toString(), completionDate.toString(), results[0][0]};
+    }
+
 private:
     int id;
     Date creationDate;
     Date completionDate;
     int pharmacyId;
+
+    Request(int id, const Date &creationDate, const Date &completionDate, int pharmacyId) :
+            id(id),
+            creationDate(creationDate),
+            completionDate(completionDate),
+            pharmacyId(pharmacyId) {}
+
+
+    static Request parseFromVector(const vector<string> &vector) {
+        int id = stoi(vector[0]);
+        Date creationDate = Date::parseFromString(vector[1]);
+        Date completionDate = Date::parseFromString(vector[2]);
+        int pharmacyId = stoi(vector[3]);
+
+        return {id, creationDate, completionDate, pharmacyId};
+    }
 
     void checkPharmacyExistence(SQLHDBC sqlhdbc) {
         ostringstream oss;
