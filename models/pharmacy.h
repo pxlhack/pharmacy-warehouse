84d4e08ab80fb35dfd31f0cb783de8eb58692c66
pxlhack <nellsouhd@gmail.com>
2023-10-04 19:51:58 +0700

    added deleteById for pharmacy.h

--- models/pharmacy.h
+++ models/pharmacy.h
@@ -19,44 +19,6 @@ public:
             address(move(address)),
             phoneNumber(move(phoneNumber)) {}
 
-    void save(SQLHDBC sqlhdbc) {
-        ostringstream oss;
-        oss << "INSERT INTO pharmacies(name, address, phone_number) VALUES ('"
-            << name << "', '" << address << "', '" << phoneNumber << "');";
-
-        string insertSql = oss.str();
-
-        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, insertSql);
-    }
-
-    static vector<Pharmacy> findAll(SQLHDBC sqlhdbc) {
-        string selectSql = "SELECT * FROM pharmacies;";
-        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, selectSql);
-
-        vector<Pharmacy> pharmacies;
-        for (const auto &row: results) {
-            Pharmacy pharmacy = parseFromVector(row);
-            pharmacies.push_back(pharmacy);
-        }
-        return pharmacies;
-    }
-
-    vector<string> toStringVector() {
-        return {to_string(this->id), this->name, this->address, this->phoneNumber};
-    }
-
-
-    static Pharmacy findById(SQLHDBC sqlhdbc, int id) {
-        ostringstream oss;
-        oss << "SELECT * FROM pharmacies WHERE id = " << id << ";";
-        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, oss.str());
-
-        if (results.empty()) {
-            throw runtime_error("Pharmacy not found");
-        }
-
-        return parseFromVector(results[0]);
-    }
 
     string getName() {
         return name;
@@ -86,6 +48,41 @@ public:
         Pharmacy::phoneNumber = phoneNumber;
     }
 
+
+    void save(SQLHDBC sqlhdbc) {
+        ostringstream oss;
+        oss << "INSERT INTO pharmacies(name, address, phone_number) VALUES ('"
+            << name << "', '" << address << "', '" << phoneNumber << "');";
+
+        string insertSql = oss.str();
+
+        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, insertSql);
+    }
+
+    static vector<Pharmacy> findAll(SQLHDBC sqlhdbc) {
+        string selectSql = "SELECT * FROM pharmacies;";
+        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, selectSql);
+
+        vector<Pharmacy> pharmacies;
+        for (const auto &row: results) {
+            Pharmacy pharmacy = parseFromVector(row);
+            pharmacies.push_back(pharmacy);
+        }
+        return pharmacies;
+    }
+
+    static Pharmacy findById(SQLHDBC sqlhdbc, int id) {
+        ostringstream oss;
+        oss << "SELECT * FROM pharmacies WHERE id = " << id << ";";
+        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, oss.str());
+
+        if (results.empty()) {
+            throw runtime_error("Pharmacy not found");
+        }
+
+        return parseFromVector(results[0]);
+    }
+
     void update(SQLHDBC sqlhdbc) {
         ostringstream oss;
         oss << "UPDATE pharmacies "
@@ -95,6 +92,22 @@ public:
             "' WHERE id = " << id << ";";
 
         SqlExecutor::executeSql(sqlhdbc, oss.str());
+    }
+
+    static void deleteById(SQLHDBC sqlhdbc, int id) {
+        if (isPharmacyExists(sqlhdbc, id)) {
+            if (!doesAnyRequestReferToPharmacy(sqlhdbc, id)) {
+                ostringstream oss;
+                oss << "DELETE FROM pharmacies "
+                       "WHERE id = " << id << ";";
+                vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, oss.str());
+            } else {
+                throw runtime_error("The pharmacy is present in the requests, it cannot be deleted");
+            }
+
+        } else {
+            throw runtime_error("Pharmacy not found");
+        }
     }
 
 private:
@@ -113,6 +126,28 @@ private:
 
     static Pharmacy parseFromVector(vector<string> vector) {
         return {stoi(vector[0]), vector[1], vector[2], vector[3]};
+    }
+
+
+    static bool isPharmacyExists(SQLHDBC sqlhdbc, int id) {
+        ostringstream oss;
+        oss << "SELECT 1 FROM pharmacies WHERE id = " << id << ";";
+        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, oss.str());
+
+        if (!results.empty() && results[0][0] == "1") {
+            return true;
+        }
+        return false;
+    }
+
+    static bool doesAnyRequestReferToPharmacy(SQLHDBC sqlhdbc, int id) {
+        ostringstream oss;
+        oss << "SELECT 1 FROM requests WHERE pharmacy_id = " << id << ";";
+        vector<vector<string>> results = SqlExecutor::executeSql(sqlhdbc, oss.str());
+        if (!results.empty() && results[0][0] == "1") {
+            return true;
+        }
+        return false;
     }
 };
 
